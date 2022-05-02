a0589575be7cafb38a3d0294deb1cb665007395c
Samuel Alene <smlalene@gmail.com>
2022-05-02 11:09:56 -0700

    creating a searchbar

--- base/views.py
+++ base/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from matplotlib.style import context
+from django.db.models import Q
 from .forms import RoomForm
 
-from .models import Room
+from .models import Room, Topic
 
 # Create your views here.
 #rooms = [
@@ -12,10 +14,19 @@ from .models import Room
 #]
 
 def home(request):
-    rooms = Room.objects.all()
-    return render(request, 'base/home.html', {
-        'rooms': rooms
-    })
+    q = request.GET.get('q') if request.GET.get('q') != None else  ''
+
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(descriptions__icontains=q) |
+        Q(name__icontains=q)
+    )
+
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+
+    context = {'rooms' : rooms, 'topics': topics, 'room_count': room_count}
+    return render(request, 'base/home.html', context)
 
 def index(request, pk):
     room = Room.objects.get(id=pk)
